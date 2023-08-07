3de77090d2d10d94040d505b29e0899aa7ed2cc9
justNikee <hk5561111212@gmail.com>
2023-08-08 00:50:31 +0530

    fetching api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
 // import Products from '.././src/components/products'
+
+
+interface Product {
+  product: string;
+  index: string;
+}
 
 function App() {
 
@@ -20,22 +27,21 @@ function App() {
 
   return (
     <>
-    <div>
-      products
+    <div className='flex flex-col justify-center items-center'>
+      <h2 className='text-xl'>Products</h2>
+      <div className='grid grid-cols-4'>
+      {products?.map((product: Product, index: number) => (
+        <div className='flex justify-start items-center flex-col gap-3' key={index}>
+          <img src={product.thumbnail} alt={product.title} />
+        <div>
+      <h2 className='text-l'>{product.title}</h2>
+      <h3 className='text-m'>{product.price}</h3>
     </div>
-      {products.map((product:any, index) => {
-        <div key={index}>
-          <img src={product.image} alt={product.title} />
-          <div>
-            <h2>
-              {product.title}
-            </h2>
-            <h3>
-              {product.price}
-            </h3>
-          </div>
-        </div>
-      })}
+  </div>
+))}
+      </div>
+
+    </div>
     </>
   )
 }
